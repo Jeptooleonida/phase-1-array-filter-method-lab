07b7358d93b496b99ce11803244b738184ee8f0d
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-06-14 15:48:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching (drivers, string){
+    return drivers.filter((result) => result.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(result => result[0] === string[0]);
+
+}
+
+function matchName(drivers, string){
+    return drivers.filter(record => record.name === string);
+
+}
